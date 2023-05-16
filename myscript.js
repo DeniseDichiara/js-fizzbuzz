2c88782e2e88bd4eaa28d6227fca410bdadf8364
Denise-Dichiara <dichiara.d@gmail.com>
2023-05-16 15:47:01 +0200

    created for cycle

---
+++ myscript.js
@@ -0,0 +1,20 @@
+//* creazione del ciclo FOR per avere 100 numeri
+
+//todo Controllo multipli
+
+//? stampo in console
+    //? se multiplo di 3 => Fizz
+    //? Se multiplo di 5 => Buzz
+    //? se multiplo di 3 e 5 => fizzBuzz
+
+
+//let divisibleByThree 
+//let divisibleByfive
+
+//* creazione del ciclo FOR per avere 100 numeri
+for (let i = 0; i <= 100 ; i ++){
+    //? creazione della condizione di divisibilità: if i / n = 0 allora è un multiplo di 
+   console.log(i);
+
+
+}

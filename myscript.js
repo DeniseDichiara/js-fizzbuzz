614f00ccbbc443af69ebeb9368948ff019176caa
Denise-Dichiara <dichiara.d@gmail.com>
2023-05-16 16:20:11 +0200

    modified if condition divisible by three and five

--- myscript.js
+++ myscript.js
@@ -14,10 +14,13 @@
 //* creazione del ciclo FOR per avere 100 numeri
 for (let i = 1; i <= 100 ; i ++){
 console.log(i);
+const divisibleByThree = 3;
 
 //? creazione della condizione di divisibilità: if i / n = 0 allora è un multiplo di 
-if (i % i ++ === 0){
-    console.log('divisible');
+if (i % 3  === 0){
+    console.log('divisible by three');
+}else if (i % 5 === 0) {
+    console.log('divisible by five');
 }
 
 }

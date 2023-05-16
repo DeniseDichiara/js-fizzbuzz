b081315ea652ddefa52a2caffad1902b25472d7d
Denise-Dichiara <dichiara.d@gmail.com>
2023-05-16 17:05:02 +0200

    modified if condition FizzBuzz

--- myscript.js
+++ myscript.js
@@ -8,15 +8,19 @@
     //? se multiplo di 3 e 5 => fizzBuzz
 
 
-//let divisibleByThree 
-//let divisibleByfive
+let fizzBuzz
+
 
 //* creazione del ciclo FOR per avere 100 numeri
 for (let i = 1; i <= 100 ; i ++){
 console.log(i);
 
 //? creazione della condizione di divisibilità: if i / n = 0 allora è un multiplo di 
-if (i % 3  === 0){
+if((i % 3 === 0) && (i % 5 === 0)){
+    console.log('FizzBuzz');
+}
+
+else if (i % 3  === 0){
     console.log('Fizz');
 }else if (i % 5 === 0) {
     console.log('Buzz');

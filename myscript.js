15c212339cecebd44a7866bbe6954472b4c1fe65
Denise-Dichiara <dichiara.d@gmail.com>
2023-05-17 08:22:48 +0200

    added BONUS 1

--- myscript.js
+++ myscript.js
@@ -10,18 +10,24 @@
 
 let fizzBuzz
 
+const divElement = document.querySelector('div');
 
-//* creazione del ciclo FOR per avere 100 numeri
 for (let i = 1; i <= 100 ; i ++){
 console.log(i);
 
-//? creazione della condizione di divisibilità: if i / n = 0 allora è un multiplo di 
 if((i % 3 === 0) && (i % 5 === 0)){
     console.log('FizzBuzz');
 }
 
+
 else if (i % 3  === 0){
     console.log('Fizz');
+    const FIZZnumber = document.createElement('p');
+    FIZZnumber.append('number: ' + i);
+
+    divElement.append(FIZZnumber);
+
+
 }else if (i % 5 === 0) {
     console.log('Buzz');
 }
